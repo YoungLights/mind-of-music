edd1a69dea637f5b65bb8e62eba1b76e8e898af4
Sascha Gotthard <hello@younglights.de>
2020-01-30 10:35:06 +0100

    First Express App

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'assets')));
+
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    partialsDir: __dirname + '/views/inc'
+}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/impressum', (req, res) => {
+    res.render('impressum');
+});
+
+app.get('/datenschutz', (req, res) => {
+    res.render('datenschutz');
+});
+
+app.listen(3000, () => {
+    console.log('App is running on port 3000....');
+});
